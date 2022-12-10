6686d8a523823a6379369328acbde890ca9fd7bf
iman <iman.bachatt@gmail.com>
2022-12-10 23:43:34 +0200

    project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [users, setUsers] = useState(null)
+
+  useEffect(() => {
+    const userRes = axios.get('http://localhost:5000/users').then(res => {
+      setUsers(res.data)
+      console.log(res.data)
+    });
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi</h1>
+      {users ? (
+        users.map((user) => (
+          <div key={user.id}>
+            <p>name: {user.name}</p>
+            <p>surname: {user.surname}</p>
+            <p>age: {user.age}</p>
+          </ div>
+        ))
+      ) : (
+        <h2>Loading</h2>
+      )}
     </div>
   );
 }
